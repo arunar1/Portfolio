8ad65ab5af2745e42c7423c17ab3879d6cd4c61e
arunar1 <arunarofficial00@gmail.com>
2023-08-19 17:16:21 +0530

    commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const workEle=document.querySelector('.work1');
+workEle.addEventListener('click',()=>{
+    document.querySelector(".workviewall").style.display = "block";
+    document.querySelector('.work1').style.display="none"
+})
