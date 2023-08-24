0c61779a74290ce6e898d752044a325369aa665b
arunar1 <arunarofficial00@gmail.com>
2023-08-24 21:44:29 +0530

    commit

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const workEle=document.querySelector('.work1');
 workEle.addEventListener('click',()=>{
+    
+    document.querySelector('.viewswork').classList.toggle("workviewall")
+    // document.querySelector('.work1').style.display="none"
+    workEle.innerHTML="view Less"
+})
+
+const writingEle=document.querySelector('.writing1');
+workEle.addEventListener('click',()=>{
     document.querySelector(".workviewall").style.display = "block";
-    document.querySelector('.work1').style.display="none"
+    // document.querySelector('.work1').style.display="none"
+    workEle.innerHTML="view Less"
 })
